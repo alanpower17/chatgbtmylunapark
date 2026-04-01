62ea389b6289c3cfe86e79ea30e91ab07833276b
alanpower17 <alan.bellotti97@gmail.com>
2026-04-02 01:48:27 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRVXc2rCK9zIgIKMitnkdQfqxZXYMcI0w",
-  authDomain: "mylunaparkchatgpt.firebaseapp.com",
-  projectId: "mylunaparkchatgpt",
-  storageBucket: "mylunaparkchatgpt.firebasestorage.app",
-  messagingSenderId: "915512401969",
-  appId: "1:915512401969:web:919e77a70ba3a9259e3510"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
 export const db = getFirestore(app);
