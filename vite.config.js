c4f1e20c59eb479fc724c7f483264c2dae09b15a
alanpower17 <alan.bellotti97@gmail.com>
2026-04-02 00:40:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://alanpower17.github.io/chatgbtmylunapark/"
+  base: "/chatgbtmylunapark/"
 })
